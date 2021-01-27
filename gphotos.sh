4913aba4e8763ff0c8486d87dd7178e64fb57312
Manuel Koell <manu.koell@mailbox.org>
2021-01-27 15:21:41 +0100

    add google photos w/ script to download them

---
+++ gphotos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+RE='\["AF1Q.*?",\["\K(.*?)(?="\,)'
+URL="https://photos.app.goo.gl/Cv5DDPJbpnh51mgZ7";
+
+PHOTOS=$(curl -L "$URL" | perl -MHTML::Entities -pe 'decode_entities($_);' | grep -oP $RE)
+
+rm -rf photos
+mkdir photos
+
+cd $_
+echo 'export default [' >> index.js
+
+for photo in $PHOTOS; do
+    curl -JLO "${photo}=d"
+    f=$(ls -t | head -1)
+
+    echo '  "'$f'", ' >> index.js
+done
+
+echo ']' >> index.js
