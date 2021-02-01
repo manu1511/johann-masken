c048ea939bc127fa497d30713fb760ae7dd33353
Manuel Koell <manu.koell@mailbox.org>
2021-02-01 18:40:09 +0100

    hide tap highlight

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { GlobalStyles } from "twin.macro";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyled = createGlobalStyle`
+  * { 
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -moz-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+`;
 
 export default function GlobalStylesComponent() {
-  return <GlobalStyles />;
+  return (
+    <>
+      <GlobalStyles />
+      <GlobalStyled />
+    </>
+  );
 }
