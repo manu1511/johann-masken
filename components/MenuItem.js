a449eb68d9fa5902f78b5e992e8362b9b1ac2adc
Manuel Koell <manu.koell@mailbox.org>
2021-02-03 22:36:56 +0100

    next link pass href

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,11 +1,14 @@
 import tw from "twin.macro";
+import Link from "next/link";
 
 const MenuItem = ({ children, name, href, title, desc }) => (
   <li tw="">
-    <a href={href} title={title} tw="block py-2">
-      {name}
-      {desc && <div tw="text-sm text-gray-500">{desc}</div>}
-    </a>
+    <Link href={href} passHref>
+      <a title={title} tw="block py-2">
+        {name}
+        {desc && <div tw="text-sm text-gray-500">{desc}</div>}
+      </a>
+    </Link>
   </li>
 );
 
