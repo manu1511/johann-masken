ec4f0b8d6083d0db519e19e113caa9e59f8412be
Manuel Koell <manu.koell@mailbox.org>
2021-02-07 16:33:23 +0100

    finish ui contact form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-// import mail from "../services/mailer";
 import tw from "twin.macro";
+
+const SUCCESS_MSG = `Nachricht gesendet. Ich werde mich so schnell es geht melden! 
+Pssst... schon gesehen? `;
+const ERROR_MSG = `Nachricht konnte nicht gesendet werden. Bitte erneut versuchen
+oder mich per Mail kontaktieren. Danke!`;
 
 const validationSchema = Yup.object({
   name: Yup.string(),
@@ -11,18 +16,18 @@ const validationSchema = Yup.object({
   text: Yup.string(),
 });
 
-const Label = ({ field, label, type, ...rest }) => (
+const Label = ({ name, label, type, ...rest }) => (
   <div tw="mb-4">
-    <label htmlFor={field} tw="block w-full text-xl font-normal">
+    <label htmlFor={name} tw="block w-full text-xl font-normal">
       {label}:
     </label>
     <Field
-      name={field}
+      name={name}
       type={type || "text"}
       tw="block w-full border-b text-xl outline-none py-1 focus:border-black"
       {...rest}
     />
-    <ErrorMessage name={field}>
+    <ErrorMessage name={name}>
       {(msg) => <p tw="text-red-500">{msg}</p>}
     </ErrorMessage>
   </div>
@@ -32,23 +37,72 @@ const ContactForm = () => (
   <Formik
     initialValues={{ name: "", mail: "", text: "" }}
     validationSchema={validationSchema}
-    onSubmit={(values, { setSubmitting }) => {
+    onSubmit={(values, actions) => {
       console.log(values);
+
+      setTimeout(() => {
+        actions.setSubmitting(false);
+        actions.resetForm();
+
+        actions.setStatus({ success: SUCCESS_MSG });
+        // actions.setStatus({ error: ERROR_MSG });
+      }, 3000);
+
+      // fetch here ...
     }}
   >
-    {({ isSubmitting }) => (
+    {({ isSubmitting, status }) => (
       <Form>
-        <Label field="name" label="Name" />
-        <Label field="mail" label="Email" type="email" placeholder="@" />
-        <Label field="text" label="Text" as="textarea" rows="5" />
+        <div
+          tw="transition-all duration-300 ease-in-out"
+          css={[isSubmitting && tw`opacity-25 cursor-wait`]}
+        >
+          <Label name="name" label="Name" disabled={isSubmitting} />
+          <Label
+            name="mail"
+            label="Email"
+            type="email"
+            placeholder="@"
+            required
+          />
+          <Label
+            name="text"
+            label="Text"
+            component="textarea"
+            rows="5"
+            placeholder="..."
+            disabled={isSubmitting}
+          />
+        </div>
 
         <button
           type="submit"
           tw="block w-full text-white bg-black text-xl p-4 focus:outline-none"
+          css={[isSubmitting && tw`cursor-wait`]}
+          disabled={isSubmitting}
         >
-          {isSubmitting && "asdfasdf"}
-          Absenden
+          {isSubmitting ? (
+            <img src="/o.svg" tw="h-7 w-auto m-auto" />
+          ) : (
+            "Senden"
+          )}
         </button>
+
+        {!isSubmitting && !!status && status.success && (
+          <p tw="p-2">
+            {status.success}
+            <a
+              href="https://www.youtube.com/watch?v=m3E8O667bd0"
+              tw="border-b border-black hover:border-none"
+              target="_blank"
+            >
+              Mein Flaggschiff
+            </a>
+          </p>
+        )}
+        {!isSubmitting && !!status && status.error && (
+          <p tw="p-2 text-red-500">{status.error}</p>
+        )}
       </Form>
     )}
   </Formik>
